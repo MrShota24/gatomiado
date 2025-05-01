4c77506219b24ae77cf47a43f793b71f05b135bc
Blass <saavedrablass01@gmail.com>
2025-05-01 07:50:49 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('FELIZ CUMPLEAÑOS :3 ').split('')
+    const titles = ('FELIZ CUMPLE ').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
